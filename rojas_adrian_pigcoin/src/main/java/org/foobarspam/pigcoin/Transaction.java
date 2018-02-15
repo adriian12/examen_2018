45bb60aa56f6427314cd31344c1e1919fe43d926
Adrián Rojas <rojasadrian12@gmail.com>
2018-02-15 17:50:38 +0100

    creacion getters y setters en transaction.java

--- rojas_adrian_pigcoin/src/main/java/org/foobarspam/pigcoin/Transaction.java
+++ rojas_adrian_pigcoin/src/main/java/org/foobarspam/pigcoin/Transaction.java
@@ -1,19 +1,58 @@
 package org.foobarspam.pigcoin;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.Signature;
 
 public class Transaction {
 	private int hash = 0;
 	private int prev_hash = 0;
-	private PublicKey pKey_sender = ""; 
-	private PublicKey pKey_recipient = "";
+	private PublicKey[] pKey_sender; 
+	private PublicKey[] pKey_recipient;
 	private int pigcoins = 0;
 	private String message = "";
-	private byte[] signature = new byte[(int) message.length()];
+//	private byte[] signature = new byte[(int) message.length()];
+	
+	
+	public Transaction(int prev_hash, PublicKey[] pKey_sender, PublicKey[] pKey_recipient, int pigcoins, String message) {
+		this.setHash(hash);
+		this.prev_hash = prev_hash;
+		this.pKey_sender = pKey_sender;
+		this.pKey_recipient = pKey_recipient;
+		this.pigcoins = pigcoins;
+		this.message = message;
+	}
+
+
+	public int getPrev_hash() {
+		return prev_hash;
+	}
+
+	public PublicKey[] getpKey_sender() {
+		return pKey_sender;
+	}
+
+	public PublicKey[] getpKey_recipient() {
+		return pKey_recipient;
+	}
+
+	public int getPigcoins() {
+		return pigcoins;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public byte[] getSignature() {
+		return signature;
+	}
+
+	public int getHash() {
+		return hash;
+	}
+
+	public void setHash(int hash) {
+		this.hash = hash;
+	}
+	
 }
 
 
@@ -41,14 +80,19 @@ public class Transaction {
 
 
 
+/*private int id = 0;
+private String direccion = "";
+private int numeroAnclajes = 0;
+private Bicicleta[] anclajes;
 
-
-
-
-
-
-
-
+//constructor
+public Estacion(int id, String direccion, int numeroAnclaje) {
+	this.id = id;
+	this.direccion = direccion;
+	this.numeroAnclajes = numeroAnclaje;
+	this.anclajes =  new Bicicleta[numeroAnclajes];
+}
+*/
 
 
 /*public PrivateKey getPrivateKey(String filename) throws Exception {
