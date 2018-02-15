ee4ff4dd409a79d78c3c3772c31391e75603fc61
Adrián Rojas <rojasadrian12@gmail.com>
2018-02-15 18:49:02 +0100

    añadido blockchainTest.java

--- rojas_adrian_pigcoin/src/main/java/org/foobarspam/Pigcoin/Wallet.java
+++ rojas_adrian_pigcoin/src/main/java/org/foobarspam/Pigcoin/Wallet.java
@@ -13,41 +13,31 @@ import java.util.Map;
  */
 
 public class Wallet {
-	byte[] address = generateKeyPair(); //get private key
-	byte[] sKey = generateKeyPair();
-	public int total_input = 0;
-	public int total_output = 0;
+	byte[] address = null; //get private key
+	byte[] sKey = null;
+	public double total_input = 0;
+	public double total_output = 0;
 	public int balance = 0;
 	
 
 	public void setSK(PrivateKey private1) {
-		
-		
+		address.hashCode();
 	}
 
 	public void setAddress(PublicKey public1) {
-		
-		
+		sKey.hashCode();
 	}
 
 	public Object getAddress() {
-		
 		return null;
 	}
 
-	public byte[] generateKeyPair() {
-		try {
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-            keyGen.initialize(1024, random);
-            KeyPair pair = keyGen.generateKeyPair();
-            return pair;
-        
-        } catch (Exception e) {
-            return null;
-        }
-
+	public void generateKeyPair() {
+        KeyPair pair = GenSig.generateKeyPair();
+        this.setSK(pair.getPrivate());
+        this.setAddress(pair.getPublic());
 	}
+	
 
 	public void loadCoins(BlockChain bChain) {
 		
@@ -80,8 +70,8 @@ public class Wallet {
 	}
 
 	public byte[] signTransaction(String message) {
-		
-		return null;
+	
+		System.out.println(message);
 	}
 
 	public void sendCoins(Object address, Double pigcoins, String message, BlockChain bChain) {
---
+++ rojas_adrian_pigcoin/src/test/java/org/foobarspam/Test/BlockChainTest.java
@@ -0,0 +1,18 @@
+package org.foobarspam.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Adrian
+ *
+ */
+public class BlockChainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
