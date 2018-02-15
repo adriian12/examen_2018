7a8c2d9eaeb9cc41f79d20b5a797f3d44762ebba
Adrián Rojas <rojasadrian12@gmail.com>
2018-02-15 18:12:10 +0100

    creada walletTest.java

---
+++ rojas_adrian_pigcoin/src/test/java/org/foobarspam/Test/TransacctionTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.foobarspam.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Adrian
+ *
+ */
+
+public class TransacctionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+}
---
+++ rojas_adrian_pigcoin/src/test/java/org/foobarspam/Test/WalletTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.foobarspam.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Adrian
+ *
+ */
+public class WalletTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
