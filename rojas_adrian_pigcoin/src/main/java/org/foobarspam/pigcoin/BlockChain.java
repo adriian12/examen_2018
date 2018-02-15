8659cad39707f885cf7a4dad364c992b4dd34b58
Adrián Rojas <rojasadrian12@gmail.com>
2018-02-15 17:23:36 +0100

    creada clase blockchain

---
+++ rojas_adrian_pigcoin/src/main/java/org/foobarspam/pigcoin/BlockChain.java
@@ -0,0 +1,20 @@
+package org.foobarspam.pigcoin;
+
+public class BlockChain {
+
+	public void addOrigin(Transaction trx) {
+		
+		
+	}
+
+	public void summarize() {
+		
+		
+	}
+
+	public void summarize(Integer position) {
+		
+		
+	}
+
+}
