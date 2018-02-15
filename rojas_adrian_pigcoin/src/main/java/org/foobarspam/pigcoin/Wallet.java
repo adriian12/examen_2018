3345e850c6796ba0cf512dfe2d8d11cd0ede1091
Adrián Rojas <rojasadrian12@gmail.com>
2018-02-15 17:18:11 +0100

    creada clase wallet.java

---
+++ rojas_adrian_pigcoin/src/main/java/org/foobarspam/pigcoin/Wallet.java
@@ -0,0 +1,70 @@
+package org.foobarspam.pigcoin;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Map;
+
+public class Wallet {
+
+	public void setSK(PrivateKey private1) {
+		
+		
+	}
+
+	public void setAddress(PublicKey public1) {
+		
+		
+	}
+
+	public Object getAddress() {
+		
+		return null;
+	}
+
+	public void generateKeyPair() {
+		
+		
+	}
+
+	public void loadCoins(BlockChain bChain) {
+		
+		
+	}
+
+	public void loadInputTransactions(BlockChain bChain) {
+		
+		
+	}
+
+	public Object getInputTransactions() {
+		
+		return null;
+	}
+
+	public void loadOutputTransactions(BlockChain bChain) {
+		
+		
+	}
+
+	public Object getOutputTransactions() {
+		
+		return null;
+	}
+
+	public Map<String, Double> collectCoins(Double pigcoins) {
+		
+		return null;
+	}
+
+	public byte[] signTransaction(String message) {
+		
+		return null;
+	}
+
+	public void sendCoins(Object address, Double pigcoins, String message, BlockChain bChain) {
+		
+		
+	}
+
+
+}
